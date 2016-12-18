5de30b4f5e8a2addc98040c90d7420ea025d7770
Meggie Smith <oleary.audio@gmail.com>
2016-12-17 19:05:23 -0500

    Lab completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,37 +25,45 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
-
+var resultA = a>b
+print (resultA)
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+var resultB = a%b
+print(resultB)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
-
+var resultC = (y*b)<=a
+print (resultC)
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
-
+var resultD = !(a>=b)
+print (resultD)
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
-
+if (a%b)==0{
+    print (true)
+}
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
+if (Double(a)/Double(b))>x {
+    print(true)
+}
 
 
 
@@ -63,15 +71,20 @@ let b = 32
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
-
-
+if (Double(y)/x)>3{
+    print (true)
+}   else {
+    print (false)
+}
 
 
 /*: Question 8
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
 // write your code here
-
+if (y>Int(x)) && ((a/b)>9){
+    print (true)
+}
 
 
 
@@ -79,6 +92,16 @@ let b = 32
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
 // write your code here
+
+func isGreater (c: Int, d: Int) -> Bool{
+    var e: Bool
+    if c > d{
+        e = true
+    }   else {
+        e = false
+    }
+    return (e)
+}
 
 
 
@@ -90,13 +113,44 @@ let b = 32
 */
 // write your code here
 
+func isForceWith(name: String) -> Bool{
+    var f: Bool
+    if name == "Luke"{
+        f = true
+    }   else if name == "Leia"{
+        f = true
+    }   else if name == "Anakin"{
+        f = true
+    }   else if name == "Obi Wan"{
+        f = true
+    }   else if name == "Yoda" {
+        f = true
+    }   else if name == "Vader"{
+        f = true
+    }   else {
+        f = false
+    }
+    return f
+}
+
 
 
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
-
+func giveMeMoney(myAccount: Double, notMyAccount: Double) -> Double{ //Need decimal places for currency, so the arguments should be Double or Float
+    var myNewAccount = myAccount
+    var notMyNewAccount = notMyAccount
+    if notMyAccount >= 10{
+        myNewAccount = myNewAccount + 10
+        notMyNewAccount = notMyNewAccount - 10
+    }
+    //print results to check
+    print ("My New Account: \(myNewAccount)")
+    
+    return myNewAccount
+}
 
 
 
